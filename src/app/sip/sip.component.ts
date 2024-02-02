c399122be1dc59b2d9dee2435be0024a45afbe78
Suryaprakash <suryaprakash1024999@gmail.com>
2024-02-02 22:46:58 +0530

    fix chart data for sip

--- src/app/sip/sip.component.ts
+++ src/app/sip/sip.component.ts
@@ -11,7 +11,7 @@ export class SipComponent  implements OnInit{
   interestRate: number = 7;
   investmentDuration: number = 12;
   totalAmount: number = 0;
-  sipResults: { year: number, totalAmount: number }[] = [];
+  sipResults: { year: number, totalAmount: number,wealthGained:number }[] = [];
   limitData: number[] = [0,55,100];
 ngOnInit(): void {
   this.calculateSIP();
@@ -21,21 +21,25 @@ ngOnInit(): void {
     this.sipResults = [];
     const monthlyRate = this.interestRate / 12 / 100;
     const totalMonths = this.investmentDuration * 12;
+    let totalInvestment = this.investmentAmount;
 
     let futureValue = this.investmentAmount;
     for (let i = 1; i <= totalMonths; i++) {
       futureValue += this.monthlyContribution;
       futureValue *= (1 + monthlyRate);
+      totalInvestment += this.monthlyContribution;
 
       if (i % 12 === 0 || i === totalMonths) {
         const yearData = {
           year: Math.ceil(i / 12),
-          totalAmount: +futureValue.toFixed(0)
+          totalAmount: +futureValue.toFixed(0),
+          wealthGained: +(futureValue - totalInvestment).toFixed(0)
         };
         this.sipResults.push(yearData);
       }
     }
-
+    const lastRec = this.sipResults[this.sipResults.length - 1];
+    this.limitData = [0,(lastRec.wealthGained*100/lastRec.totalAmount),100];
     this.totalAmount = futureValue;
   }
 
